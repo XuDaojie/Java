025dbe348f626044715495b479a8a24281441bcb
XuDaojie <xudaojieg@gmail.com>
2017-05-16 13:49:00 +0800

    Redis

--- app/src/main/java/com/xdj/javaee/controller/RedisController.java
+++ app/src/main/java/com/xdj/javaee/controller/RedisController.java
@@ -1,25 +1,72 @@
 package com.xdj.javaee.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
 
 /**
  * Created by xdj on 2017/5/16.
  */
-@Controller
+@RestController
+@RequestMapping("redis")
 public class RedisController {
 
     @RequestMapping("set")
     public String redisSet(@RequestParam("key") String key,
                            @RequestParam("value") String val) {
-        return "greeting";
+        JedisPool pool = new JedisPool(new JedisPoolConfig(), "192.168.199.211");
+
+        Jedis jedis = pool.getResource();
+        String result = jedis.set(key, val);
+        jedis.close();
+
+        pool.destroy();
+
+        return result;
     }
 
-    @RequestMapping(value = "springMVC/get")
-    public String redisGet(@RequestParam("key") String key, Model model) {
-        model.addAttribute("name", key);
-        return "greeting"; // 视图的地址
+    @RequestMapping("get")
+    public String redisGet(@RequestParam("key") String key) {
+        JedisPool pool = new JedisPool(new JedisPoolConfig(), "192.168.199.211");
+
+        Jedis jedis = pool.getResource();
+        String value = jedis.get(key);
+        jedis.close();
+
+        pool.destroy();
+        return value;
+    }
+
+    @RequestMapping("lpush")
+    public String redisLpush(@RequestParam("key") String key,
+                             @RequestParam("value") String val) {
+        JedisPool pool = new JedisPool(new JedisPoolConfig(), "192.168.199.211");
+
+        Jedis jedis = pool.getResource();
+        Long size = jedis.lpush(key, val);
+        jedis.close();
+
+        pool.destroy();
+        return size + "";
+    }
+
+    @RequestMapping("lrange")
+    public String redisGet(@RequestParam("key") String key,
+                           @RequestParam(value = "start", defaultValue = "0") Long start,
+                           @RequestParam(value = "end", defaultValue = "-1") Long end) {
+        JedisPool pool = new JedisPool(new JedisPoolConfig(), "192.168.199.211");
+
+        Jedis jedis = pool.getResource();
+        List<String> values = jedis.lrange(key, start, end);
+        jedis.close();
+
+        pool.destroy();
+        return values.toString();
     }
 }
