2c1035d3785fed1f5b7a0e87c23a51f578baaa9a
XuDaojie <xudaojieg@gmail.com>
2017-05-18 13:44:14 +0800

    Spring Boot

--- springboot/src/main/java/com/xdj/springboot/DemoApplication.java
+++ springboot/src/main/java/com/xdj/springboot/DemoApplication.java
@@ -1,17 +1,17 @@
 package com.xdj.springboot;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by xdj on 2017/5/18.
  */
-//@SpringBootApplication
-@EnableAutoConfiguration
+@SpringBootApplication // 开启组件扫描和自动配置
+//@EnableAutoConfiguration
 @RestController
-public class Application {
+public class DemoApplication {
 
     @RequestMapping("/")
     String home() {
@@ -19,6 +19,6 @@ public class Application {
     }
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(DemoApplication.class, args);
     }
 }
---
+++ springboot/src/test/java/com/xdj/springboot/DemoApplicationTest.java
@@ -0,0 +1,27 @@
+package com.xdj.springboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+
+/**
+ * Created by xdj on 2017/5/18.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = DemoApplicationTest.class) // 通过Spring Boot 加载上下文
+@WebAppConfiguration
+public class DemoApplicationTest {
+
+    @Test
+    public void contextLoads() {
+    }
+
+    @Test
+    public void home() throws Exception {
+
+    }
+
+}
