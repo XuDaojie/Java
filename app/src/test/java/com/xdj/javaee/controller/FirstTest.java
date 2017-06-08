1366bf8a248e8b202d522652d1b68082c13d70a2
XuDaojie <xudaojieg@gmail.com>
2017-06-08 15:46:42 +0800

    First JUnit

---
+++ app/src/test/java/com/xdj/javaee/controller/FirstTest.java
@@ -0,0 +1,16 @@
+package com.xdj.javaee.controller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by xdj on 2017/6/8.
+ */
+public class FirstTest {
+
+    @Test
+    public void myFirstTest() {
+        assertEquals(2, 1 + 1);
+    }
+}
