a8be7221061e20a47f6704cbab3f459556699429
XuDaojie <xudaojieg@gmail.com>
2018-04-15 11:22:24 +0800

    升级Gradle至4.6

---
+++ shiro/src/test/java/io/github/xudaojie/shiro/example2/LoginLogoutTest.java
@@ -0,0 +1,161 @@
+package io.github.xudaojie.shiro.example2;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.IncorrectCredentialsException;
+import org.apache.shiro.authc.LockedAccountException;
+import org.apache.shiro.authc.UnknownAccountException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.config.IniSecurityManagerFactory;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.util.Factory;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class LoginLogoutTest {
+
+    @Test
+    public void testHelloWorld() {
+        //1.或SecurityManager工厂，初始化ini文件
+        Factory<SecurityManager> factory = new IniSecurityManagerFactory("classpath:shiro.ini");
+        SecurityManager securityManager = factory.getInstance();
+        SecurityUtils.setSecurityManager(securityManager);
+
+        Subject currentUser = SecurityUtils.getSubject();
+
+        if (!currentUser.isAuthenticated()) {
+            UsernamePasswordToken token = new UsernamePasswordToken("guest", "guest");
+            token.setRememberMe(true);
+            try {
+                currentUser.login(token);
+                //if no exception, that's it, we're done!
+            } catch (UnknownAccountException uae) {
+                //username wasn't in the system, show them an error message?
+                uae.printStackTrace();
+            } catch (IncorrectCredentialsException ice) {
+                //password didn't match, try again?
+                ice.printStackTrace();
+            } catch (LockedAccountException lae) {
+                //account for that username is locked - can't login.  Show them a message?
+                lae.printStackTrace();
+            } catch (AuthenticationException ae) {
+                //unexpected condition - error?
+                ae.printStackTrace();
+            }
+        }
+        Assert.assertEquals(true, currentUser.isAuthenticated()); //断言用户已经登录
+
+        currentUser.logout();
+    }
+
+    @Test
+    public void testCustomRealm() {
+        //1.或SecurityManager工厂，初始化ini文件
+        Factory<SecurityManager> factory = new IniSecurityManagerFactory("classpath:shiro-realm.ini");
+        SecurityManager securityManager = factory.getInstance();
+        SecurityUtils.setSecurityManager(securityManager);
+
+        Subject currentUser = SecurityUtils.getSubject();
+
+        if (!currentUser.isAuthenticated()) {
+            UsernamePasswordToken token = new UsernamePasswordToken("!guest", "guest");
+            token.setRememberMe(true);
+            try {
+                currentUser.login(token);
+                //if no exception, that's it, we're done!
+            } catch (UnknownAccountException uae) {
+                //username wasn't in the system, show them an error message?
+                uae.printStackTrace();
+            } catch (IncorrectCredentialsException ice) {
+                //password didn't match, try again?
+                ice.printStackTrace();
+            } catch (LockedAccountException lae) {
+                //account for that username is locked - can't login.  Show them a message?
+                lae.printStackTrace();
+            } catch (AuthenticationException ae) {
+                //unexpected condition - error?
+                ae.printStackTrace();
+            }
+        }
+        Assert.assertEquals(true, currentUser.isAuthenticated()); //断言用户已经登录
+
+        currentUser.logout();
+
+    }
+
+    @Test
+    public void testCustomMultiRealm() {
+        //1.或SecurityManager工厂，初始化ini文件
+        Factory<SecurityManager> factory = new IniSecurityManagerFactory("classpath:shiro-multi-realm.ini");
+        SecurityManager securityManager = factory.getInstance();
+        SecurityUtils.setSecurityManager(securityManager);
+
+        Subject currentUser = SecurityUtils.getSubject();
+
+        if (!currentUser.isAuthenticated()) {
+            UsernamePasswordToken token = new UsernamePasswordToken("root", "root");
+            token.setRememberMe(true);
+            try {
+                currentUser.login(token);
+                //if no exception, that's it, we're done!
+            } catch (UnknownAccountException uae) {
+                //username wasn't in the system, show them an error message?
+                uae.printStackTrace();
+            } catch (IncorrectCredentialsException ice) {
+                //password didn't match, try again?
+                ice.printStackTrace();
+            } catch (LockedAccountException lae) {
+                //account for that username is locked - can't login.  Show them a message?
+                lae.printStackTrace();
+            } catch (AuthenticationException ae) {
+                //unexpected condition - error?
+                ae.printStackTrace();
+            }
+        }
+        Assert.assertEquals(true, currentUser.isAuthenticated()); //断言用户已经登录
+
+        currentUser.logout();
+
+    }
+
+    @Test
+    public void testCustomJDBCRealm() {
+        //1.或SecurityManager工厂，初始化ini文件
+        Factory<SecurityManager> factory = new IniSecurityManagerFactory("classpath:shiro-jdbc-realm.ini");
+        SecurityManager securityManager = factory.getInstance();
+        SecurityUtils.setSecurityManager(securityManager);
+
+        Subject currentUser = SecurityUtils.getSubject();
+
+        if (!currentUser.isAuthenticated()) {
+            UsernamePasswordToken token = new UsernamePasswordToken("root", "root");
+            token.setRememberMe(true);
+            try {
+                currentUser.login(token);
+                //if no exception, that's it, we're done!
+            } catch (UnknownAccountException uae) {
+                //username wasn't in the system, show them an error message?
+                uae.printStackTrace();
+            } catch (IncorrectCredentialsException ice) {
+                //password didn't match, try again?
+                ice.printStackTrace();
+            } catch (LockedAccountException lae) {
+                //account for that username is locked - can't login.  Show them a message?
+                lae.printStackTrace();
+            } catch (AuthenticationException ae) {
+                //unexpected condition - error?
+                ae.printStackTrace();
+            }
+        }
+        Assert.assertEquals(true, currentUser.isAuthenticated()); //断言用户已经登录
+
+        currentUser.logout();
+
+    }
+
+    @Test
+    public void authorizations() {
+
+    }
+}
---
+++ shiro/src/test/java/io/github/xudaojie/shiro/example2/realm/MyRealm2.java
@@ -0,0 +1,35 @@
+package io.github.xudaojie.shiro.example2.realm;
+
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.IncorrectCredentialsException;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
+import org.apache.shiro.authc.UnknownAccountException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.realm.Realm;
+
+public class MyRealm2 implements Realm {
+    @Override
+    public String getName() {
+        return "myRealm1";
+    }
+
+    @Override
+    public boolean supports(AuthenticationToken token) {
+        return token instanceof UsernamePasswordToken;
+    }
+
+    @Override
+    public AuthenticationInfo getAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+        String username = token.getPrincipal().toString();
+        String password = token.getPrincipal().toString();
+        if (!"root".equals(username)) {
+            throw new UnknownAccountException(); //如果用户名错误
+        } else if (!"root".equals(password)) {
+            throw new IncorrectCredentialsException(); //如果密码错误
+        }
+        //身份验证成功
+        return new SimpleAuthenticationInfo(username, password, getName());
+    }
+}
