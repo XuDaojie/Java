6c88b5785f43681e73c94ec68543a1b200ce9eb1
XuDaojie <xudaojieg@gmail.com>
2020-06-19 15:00:03 +0800

    ...

---
+++ spring/spring-boot-2.x/src/main/java/io/github/xudaojie/spring/boot/Application2x.java
@@ -0,0 +1,16 @@
+package io.github.xudaojie.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xdj
+ */
+@SpringBootApplication
+public class Application2x {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application2x.class);
+    }
+
+}
