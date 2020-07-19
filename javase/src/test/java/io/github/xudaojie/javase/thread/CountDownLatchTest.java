0a7886b1c2ba53c01f0d170578997d9dea84188d
XuDaojie <xudaojieg@gmail.com>
2020-07-19 16:26:35 +0800

    Add CountDownLatchTest

---
+++ javase/src/test/java/io/github/xudaojie/javase/thread/CountDownLatchTest.java
@@ -0,0 +1,35 @@
+package io.github.xudaojie.javase.thread;
+
+import org.junit.Test;
+
+import java.util.concurrent.CountDownLatch;
+
+public class CountDownLatchTest {
+
+    /**
+     * 一个同步辅助类，在完成一组正在其他线程中执行的操作之前，它允许一个或多个线程一直等待。
+     */
+    @Test
+    public void worker1() throws InterruptedException {
+        CountDownLatch countDownLatch = new CountDownLatch(1);
+
+        new Thread(){
+            @Override
+            public void run() {
+                super.run();
+                try {
+                    Thread.sleep(5000);
+                    countDownLatch.countDown();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+
+        System.out.println("可执行其他工作");
+        System.out.println("等待其他线程执行完毕");
+        countDownLatch.await();
+
+        System.out.println("任务执行完毕");
+    }
+}
