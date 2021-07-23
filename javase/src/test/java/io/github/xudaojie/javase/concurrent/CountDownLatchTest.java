6a39da9747cb716c976bf58c2ed3d1862ad7b5f3
XuDaojie <xudaojieg@gmail.com>
2021-07-23 16:03:36 +0800

    update CountDownLatchTest

--- javase/src/test/java/io/github/xudaojie/javase/concurrent/CountDownLatchTest.java
+++ javase/src/test/java/io/github/xudaojie/javase/concurrent/CountDownLatchTest.java
@@ -8,18 +8,27 @@ public class CountDownLatchTest {
 
     /**
      * 一个同步辅助类，在完成一组正在其他线程中执行的操作之前，它允许一个或多个线程一直等待。
+     *
+     * worker1 线程任务执行完毕后才继续执行 main 线程任务
+     *
+     * +--------------------------------------------------------------------------------------+
+     * |   worker:    worker.start()         --------------------> countDownLatch.countDown() |
+     * |   main:      countDownLatch.await() --------------------> downSomething()            |
+     * |                           lock，直到worker线程执行完毕并将CountDownLatch的值变为0        |
+     * +--------------------------------------------------------------------------------------+
      */
     @Test
     public void worker1() throws InterruptedException {
-        CountDownLatch countDownLatch = new CountDownLatch(1);
+        CountDownLatch latch = new CountDownLatch(1);
 
-        new Thread(){
+        new Thread("worker") {
             @Override
             public void run() {
                 super.run();
                 try {
-                    Thread.sleep(50000);
-                    countDownLatch.countDown();
+                    Thread.sleep(3000);
+                    System.out.println(Thread.currentThread().getName() + " 任务执行完毕");
+                    latch.countDown();
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
@@ -27,9 +36,9 @@ public class CountDownLatchTest {
         }.start();
 
         System.out.println("可执行其他工作");
-        System.out.println("等待其他线程执行完毕");
-        countDownLatch.await();
+        System.out.println("等待 worker 线程执行完毕");
+        latch.await();
 
-        System.out.println("任务执行完毕");
+        System.out.println(Thread.currentThread().getName() + " 任务执行完毕");
     }
 }
