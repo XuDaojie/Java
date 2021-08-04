88ea12f1f7a4868824dd5663151ad9b171c6db04
XuDaojie <xudaojieg@gmail.com>
2021-08-04 13:42:25 +0800

    add lang-groovy

---
+++ lang-groovy/src/main/groovy/First.groovy
@@ -0,0 +1,18 @@
+class First {
+    static void main(String[] args) {
+        First f = new First()
+        f.echo("Hello World!")
+    }
+
+    void echo(Object a) {
+        if (a instanceof String) {
+            String str = a
+            println(a)
+        }
+        println("---")
+        String[] aa = new String()[2]
+        for (String aaa : aa) {
+
+        }
+    }
+}
